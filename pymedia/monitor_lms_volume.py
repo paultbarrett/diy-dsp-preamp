eab698fc38c2b23cb6cfa6ea47f6984435a48309
taradiddles <taradiddles@users.noreply.github.com>
2022-12-05 18:17:15 +0200

    add comments

--- pymedia/monitor_lms_volume.py
+++ pymedia/monitor_lms_volume.py
@@ -24,20 +24,27 @@ logging.basicConfig(level=logging.DEBUG)
 # ---------------------
 
 def cdsp_set_volume(vol, _redis):
-    """Set CamillaDSP volume via redis action.
-    """
+    """Set CamillaDSP volume via redis action."""
+    # Pass 'NO_PLAYER_VOL_UPDATE' to avoid potential "feedback loops";
+    # otherwise pymedia_cdsp would update LMS' volume when receiving a volume
+    # change action - if volumes aren't identical (eg. because of different
+    # implementations of volume change in LMS and pymedia_cdsp) then there would
+    # be an infinite back-and-forth.
     logging.info("Action - set CDSP volume to %s", vol)
     _redis.send_action('CDSP', f"volume_perc:{vol}:NO_PLAYER_VOL_UPDATE")
 
-
 def receive_volume_event(_socket, player_id, callback, cb_args=()):
 
+    # sample (we use unquote() to convert %3A to ':'):
     # 13%3A89%3A0e%3Ac8%3A1d%3Aa5 mixer volume 50
+    # 13%3A89%3A0e%3Ac8%3A1d%3Aa5 mixer volume +5
+    # ->
+    # 13:89:0e:c8:1d:a5 mixer volume 60
+    # 13:89:0e:c8:1d:a5 mixer volume -5
     regex = re.compile("^" + player_id + r" mixer volume ([+\-]?\d+)$")
 
-    # subscribe to volume changes
+    # "subscribe" to LMS volume changes
     _socket.send("subscribe mixer\r".encode("UTF-8"))
-
 
     while True:
         data = _socket.recv(4096)
