821b8227d98650e7b7e5c7da05e144f3924abf4e
taradiddles <taradiddles@users.noreply.github.com>
2022-12-08 14:22:15 +0200

    don't use an intermediate redis function

--- pymedia/gpios.py
+++ pymedia/gpios.py
@@ -17,10 +17,6 @@ from pymedia_const import REDIS_SERVER, REDIS_PORT, REDIS_DB
 
 # ----------------
 
-def redis_action(_redis, name, action):
-    """Send (publish) a pymedia style redis action."""
-    _redis.send_action(name, action)
-
 def manage_status_led(o_pin, _redis):
     """Provide visual feedback of CamillaDSP status.
 
@@ -62,9 +58,9 @@ if __name__ == '__main__':
     panel_push_btn = pymedia_gpio.GpioInputPin(
             gpiochip1,
             23,
-            cb_pressed=redis_action,
+            cb_pressed=_redis.send_action,
             cb_pressed_args=(_redis, "CDSP", "next_config"),
-            cb_held=redis_action,
+            cb_held=_redis.send_action,
             cb_held_args=(_redis, "CDSP", "first_config"),
             )
     threads.add_thread(panel_push_btn.th_wait)
@@ -73,8 +69,8 @@ if __name__ == '__main__':
     encoder_push_btn = pymedia_gpio.GpioInputPin(
             gpiochip2,
             4,
-            cb_pressed=redis_action,
-            cb_pressed_args=(_redis, "CDSP", "toggle_mute"),
+            cb_pressed=_redis.send_action,
+            cb_pressed_args=("CDSP", "toggle_mute"),
             )
     threads.add_thread(encoder_push_btn.th_wait)
 
