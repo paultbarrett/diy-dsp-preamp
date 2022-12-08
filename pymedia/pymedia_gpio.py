920d7869ae686784bd3dab14fa674189797d28e7
taradiddles <taradiddles@users.noreply.github.com>
2022-12-08 13:59:39 +0200

    add "input held" function

--- pymedia/pymedia_gpio.py
+++ pymedia/pymedia_gpio.py
@@ -9,65 +9,181 @@ import threading
 
 import gpiod
 
-from pymedia_utils import logging, Log
+from pymedia_utils import logging, Log, LOGFORMAT, LOGFORMAT_DATE
 
 # ---------------------
 
-BUTTON_PRESSED_DISCARD_TIME_WINDOW = 0.6
+DEBOUNCE_DELAY = 0.02
+HELD_TIME = 2
 
 # ----------------
 
-def wait_input_pin(gpiochip, pin, callback, threaded_callback=False,
-                   timeout=BUTTON_PRESSED_DISCARD_TIME_WINDOW,
-                   cb_args=()):
-    """Loop / run callback on pin change (interrupt based).
+class GpioInputPin(metaclass=Log):
 
-    Blocking - should be run in a thread.
-    """
-    line = gpiochip.get_line(pin)
-    # with a pull up, at rest a gpio's value is 1; falling edge indicates
-    # the input was activated; use LINE_REQ_EV_RISING_EDGE otherwise
-    line.request(consumer="wait_events",
-                 type=gpiod.LINE_REQ_EV_FALLING_EDGE)
+    def __init__(self,
+                 gpiochip,
+                 pin,
+                 cb_pressed=None,
+                 cb_pressed_args=(),
+                 cb_held=None,
+                 cb_held_args=(),
+                 threaded_callback=False,
+                 debounce_delay=DEBOUNCE_DELAY,
+                 held_time=HELD_TIME,
+                 ):
 
-    last_press_time = 0
+        # overly complex way to change the logger format to display the
+        # gpiochip/pin to help debugging
+        self._log = logging.getLogger()
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(f"{LOGFORMAT} ({gpiochip.name()}:{pin})",
+                                      datefmt=LOGFORMAT_DATE)
+        handler.setFormatter(formatter)
+        self._log.handlers = []    # logger.propagate = False
+        self._log.addHandler(handler)
 
-    while True:
-        ev_line = line.event_wait(sec=1)
-        if not ev_line:
-            continue
+        if not cb_pressed and not cb_held:
+            raise Exception("no pressed/held callback defined !")
 
-        event = line.event_read()
-        if event.type != gpiod.LineEvent.FALLING_EDGE:
-            raise TypeError('Invalid event type')
+        try:
+            if gpiochip is None:
+                self._log.error("gpiochip not found")
+                raise SystemExit
+            self._line = gpiochip.get_line(pin)
+            self._line.request(consumer="wait_events",
+                               type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+        except Exception as ex:
+            self._log.error("Error: %s", ex)
+            raise SystemExit from ex
 
-        if time.time() - last_press_time < timeout:
-            logging.info("button pressed within %ds - ignoring",
-                         timeout)
-            continue
+        self._cb_pressed = cb_pressed
+        self._cb_pressed_args = cb_pressed_args
+        self._cb_held = cb_held
+        self._cb_held_args = cb_held_args
 
-        last_press_time = time.time()
+        self._threaded_callback = threaded_callback
+        self._held_time = held_time
 
-        if callback is not None:
-            logging.debug("running callback | thread: %s",
-                            threaded_callback)
-            if threaded_callback:
-                thread = threading.Thread(target=callback,
-                                          args=(*cb_args,))
-                thread.daemon = True
-                thread.start()
+        self.th_wait = threading.Thread(target=self._wait_input,
+                                        args=(debounce_delay,))
+        self.th_wait.daemon = True
+        self._cb_held_thread_ev = threading.Event()
+
+        self._cb_held_complete = False
+
+        self._label = f"{gpiochip.name()}:{pin}"
+
+    def get_value(self):
+        return self._line.get_value()
+
+    def _run_cb_held_timer(self):
+        self._cb_held_complete = False
+        cancel = self._cb_held_thread_ev.wait(self._held_time)
+        # run callback if the task wasn't "cancelled" ; the input shouldn't be
+        # released by the time the timer expires (0: button is pressed)
+        if self.get_value() == 0 and not cancel:
+            self._cb_held_complete = True
+            self._cb_held(*self._cb_held_args)
+        else:
+            self._log.debug("input was released while waiting or thread was"
+                          " cancelled - won't run callback")
+
+    def _wait_input(self, debounce_delay):
+        """Loop / run callback on pin change (interrupt based).
+
+        Blocking - should be run in a thread.
+        """
+        last_valid_event_time = 0
+        last_event = None
+
+        cb_pressed_thread = None
+        cb_held_thread = None
+
+        while True:
+            ev_line = self._line.event_wait(sec=1)
+            if not ev_line:
+                continue
+
+            event = self._line.event_read()
+
+            # with a pull up, at rest a gpio's value is 1
+            # -> FALLING_EDGE: the input was activated (ie. button pressed)
+            # -> RISING_EDGE: the input was deactivated (ie. button released)
+
+            if event.type == gpiod.LineEvent.FALLING_EDGE:
+                self._log.debug("Falling edge - value is True")
+                pressed = True
+            elif event.type == gpiod.LineEvent.RISING_EDGE:
+                self._log.debug("Rising Edge - value is False")
+                pressed = False
             else:
-                callback(*cb_args)
+                raise TypeError('Invalid event type')
+
+            # it shouldn't be possible to receive two consecutive event types
+            # (eg. "Falling edge" then "Falling edge" again) yet this often
+            # happens. HW or SW bug ?
+            if last_event == event.type:
+                self._log.debug("Last event type was identical - skipping")
+                continue
+            last_event = event.type
+
+            if time.time() - last_valid_event_time < debounce_delay:
+                self._log.debug("button event %s edge within %ss - ignoring",
+                             "Falling" if pressed else "Rising",
+                             debounce_delay)
+                continue
+            last_valid_event_time = time.time()
+
+            run_cb_pressed = False
+            run_cb_held = False
+
+            if pressed:
+                self._log.debug("input activated %s", self._label)
+                if self._cb_held:
+                    if cb_held_thread and cb_held_thread.is_alive():
+                        self._log.debug("cb_held thread already started -"
+                                      "bounce ?")
+                        continue
+                    run_cb_held = True
+                elif self._cb_pressed:
+                    run_cb_pressed = True
+
+            else:
+                self._log.debug("input deactivated, %s", self._label)
+                # cancel a cb_held task if running
+                if self._cb_held:
+                    if cb_held_thread and cb_held_thread.is_alive():
+                        self._log.debug("Cancelling cb_held thread")
+                        self._cb_held_thread_ev.set()
+                        self._cb_held_thread_ev.clear()
+
+                    if self._cb_pressed and not self._cb_held_complete:
+                        if cb_pressed_thread and cb_pressed_thread.is_alive():
+                            self._log.debug("cb_pressed thread already started")
+                        else:
+                            run_cb_pressed = True
+
+            if run_cb_pressed:
+                self._log.debug("Starting cb_pressed thread")
+                cb_pressed_thread = threading.Thread(target=self._cb_pressed,
+                                                     args=self._cb_pressed_args)
+                cb_pressed_thread.daemon = True
+                cb_pressed_thread.start()
+
+            if run_cb_held:
+                self._log.debug("Starting cb_held timer thread")
+                cb_held_thread = threading.Thread(
+                        target=self._run_cb_held_timer)
+                cb_held_thread.daemon = True
+                cb_held_thread.start()
 
 
 class GpioOutputPin(metaclass=Log):
     def __init__(self, gpiochip, pin):
-        logging.debug("args gpiochip=%s pin=%d", gpiochip, pin)
         try:
             if gpiochip is None:
                 logging.error("gpiochip not found")
                 raise SystemExit
-            logging.debug("type %s %s", type(gpiochip), type(pin))
             self._line = gpiochip.get_line(pin)
             self._line.request(consumer="pymedia", type=gpiod.LINE_REQ_DIR_OUT)
         except Exception as ex:
