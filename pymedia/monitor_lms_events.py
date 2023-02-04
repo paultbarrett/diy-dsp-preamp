3a9424dd5e78e92a9db01d2c939200ba69d53e3a
taradiddles <taradiddles@users.noreply.github.com>
2023-02-04 09:32:27 +0200

    overhaul - better handling of disconnects/errors

--- pymedia/monitor_lms_events.py
+++ pymedia/monitor_lms_events.py
@@ -8,8 +8,13 @@ import socket
 import re
 from urllib.parse import unquote
 
+import threading
+import time
+
 import pymedia_redis
 import pymedia_logger
+
+from pymedia_utils import SimpleThreads
 
 from pymedia_const import REDIS_SERVER, REDIS_PORT, REDIS_DB
 
@@ -22,6 +27,9 @@ LMS_SERVER_PORT = 9090
 LMS_PLAYERID = "13:89:0e:c8:1d:a5"
 LMS_SUBSCRIBE = "mixer,playlist,play,pause,stop"
 
+LMS_PING_INTERVAL = 4    # MUST be less than LMS_SOCKET_TIMEOUT
+LMS_SOCKET_TIMEOUT = 10
+
 VOL_EVENT_KEYS = (
         'ROTARY_ENCODER:last_volume_event',
         )
@@ -29,7 +37,7 @@ VOL_EVENT_MAX_AGE = 2
 
 # ---------------------
 
-def cdsp_set_volume(vol, _redis):
+def cdsp_set_volume(_redis, vol):
     """Set CamillaDSP volume via redis action."""
     # don't update player volume if volume was changed by other means
     # within the last VOL_EVENT_MAX_AGE seconds
@@ -46,46 +54,120 @@ def cdsp_set_volume(vol, _redis):
     logger.info("Action - set CDSP volume to %s", vol)
     _redis.send_action('CDSP', f"volume_perc:{vol}:NO_PLAYER_VOL_UPDATE")
 
-def parse_events(_socket, player_id, _redis):
-    """Receive / parse events and trigger actions."""
+def player_update_action(_redis):
+    """Send a redis action to trigger a player stats update."""
+    _redis.send_action('PLAYER', "update")
 
-    # sample (we use unquote() to convert %3A to ':'):
-    # 13%3A89%3A0e%3Ac8%3A1d%3Aa5 mixer volume 50
-    # 13%3A89%3A0e%3Ac8%3A1d%3Aa5 mixer volume -5
-    # ->
-    # 13:89:0e:c8:1d:a5 mixer volume 50
-    # 13:89:0e:c8:1d:a5 mixer volume -5
-    re_vol = re.compile("^" + player_id + r" mixer volume ([+\-]?\d+(\.\d+)?)$")
+class LmsCliVol():
+    def __init__(self, server, port, playerid,
+                 f_vol, f_vol_args,
+                 f_default, f_default_args,
+                 ping_interval=LMS_PING_INTERVAL,
+                 socket_timeout=LMS_SOCKET_TIMEOUT):
 
-    while True:
-        data = _socket.recv(4096)
-        line = unquote(data.decode("UTF-8").strip())
-        logger.debug("received %s", line)
-        re_vol_match = re_vol.match(line)
-        if re_vol_match:
-            vol = re_vol_match.group(1)
-            logger.info("Volume changed to %s for player %s", vol,
-                         LMS_PLAYERID)
-            cdsp_set_volume(vol, _redis)
-        else:
-            logger.debug("Action - PLAYER:update (received '%s')", line)
-            _redis.send_action('PLAYER', "update")
+        self._log = pymedia_logger.get_logger(__class__.__name__)
+        self.playerid = playerid
+        self._redis = _redis
+
+        self.f_vol = f_vol
+        self.f_vol_args = f_vol_args
+
+        self.f_default = f_default
+        self.f_default_args = f_default_args
+
+        self.th_ev_stop = threading.Event()
+
+        self.threads = SimpleThreads()
+        self.threads.add_target(self.ping, ping_interval)
+        self.threads.add_target(self.parse_rcv)
+        #self.threads.add_target(self.connect, server, port)
+
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.settimeout(socket_timeout)
+
+        self._log.info("Connecting to %s:%s", server, port)
+        try:
+            self._sock.connect((server, port))
+        except Exception as ex:
+            self._log.error(ex)
+            self.cleanup()
+            raise SystemExit from ex
+
+        self._log.info("Subscribing")
+        self._sock.send(f"subscribe {LMS_SUBSCRIBE}\r".encode("UTF-8"))
+
+        self.threads.start()
+
+    def ping(self, ping_interval):
+        """Ping (query version) at regular intervals."""
+        while not self.th_ev_stop.is_set():
+            self._log.info("ping")
+            self._sock.send("version ?\r".encode("UTF-8"))
+            time.sleep(ping_interval)
+
+    def parse_rcv(self):
+        """Receive / parse events and trigger actions."""
+
+        # sample (we use unquote() to convert %3A to ':'):
+        # 13%3A89%3A0e%3Ac8%3A1d%3Aa5 mixer volume 50
+        # 13%3A89%3A0e%3Ac8%3A1d%3Aa5 mixer volume -5
+        # ->
+        # 13:89:0e:c8:1d:a5 mixer volume 50
+        # 13:89:0e:c8:1d:a5 mixer volume -5
+        re_vol = re.compile("^" + self.playerid
+                            + r" mixer volume ([+\-]?\d+(\.\d+)?)$")
+
+        while not self.th_ev_stop.is_set():
+
+            try:
+                data = self._sock.recv(4096)
+            except Exception as ex:
+                self._log.error(ex)
+                self.cleanup()
+                raise SystemExit from ex
+
+            line = unquote(data.decode("UTF-8").strip())
+            self._log.debug("received '%s'", line)
+
+            if not line:
+                self._log.error("Empty line received - connection closed ?")
+                self.cleanup()
+                raise SystemExit
+
+            if line.startswith("subscribe") or line.startswith("version"):
+                continue
+
+            re_vol_match = re_vol.match(line)
+            if re_vol_match:
+                vol = re_vol_match.group(1)
+                self._log.info("Volume changed to %s for player %s", vol,
+                        LMS_PLAYERID)
+                self.f_vol(self.f_vol_args, vol)
+            else:
+                self._log.info("Action - PLAYER:update (received '%s')", line)
+                self.f_default(self.f_default_args)
+
+    def cleanup(self):
+        self.th_ev_stop.set()
+        time.sleep(2)
+        self._sock.close()
+
 
 
 # ---------------------
 
 if __name__ == '__main__':
 
-    redis = pymedia_redis.RedisHelper(REDIS_SERVER, REDIS_PORT, REDIS_DB,
+    _redis = pymedia_redis.RedisHelper(REDIS_SERVER, REDIS_PORT, REDIS_DB,
                                       'PLAYER_CHANNEL')
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #sock.settimeout(3) # 3 second timeout on commands
-    sock.connect((LMS_SERVER, LMS_SERVER_PORT))
+    lms_cli_vol = LmsCliVol(LMS_SERVER, LMS_SERVER_PORT, LMS_PLAYERID,
+                            cdsp_set_volume, (_redis,),
+                            player_update_action, (_redis)
+                            )
 
     try:
-        sock.send(f"subscribe {LMS_SUBSCRIBE}\r".encode("UTF-8"))
-        parse_events(sock, LMS_PLAYERID, redis)
+        lms_cli_vol.threads.join()
     except KeyboardInterrupt:
         print("Received KeyboardInterrupt, shutting down...")
-        sock.close()
+        lms_cli_vol.cleanup()
